09c8c32d966ce545832cdefbb205376683a6dbac
CS5859 <cjs2032@cox.net>
2016-07-20 14:36:24 -0400

    Restructuring
    Added new folder and BG is working

--- public/Javascript/functions.js
+++ public/Javascript/functions.js
@@ -1,7 +1,5 @@
 function bgPicker() 
 {
     var bgNum = Math.floor(Math.random() * 5) + 1;
-    var b = document.getElementById("demo");
-    b.innerHTML = bgNum;
     document.body.style.backgroundImage = "url('Pictures/bg"+bgNum+".jpg')";
 }
