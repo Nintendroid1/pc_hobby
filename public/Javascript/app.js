971123c6c91cc955e8dd1cb5fad99aea084b104d
Chris Story <storytimesolutions@gmail.com>
2016-07-25 15:13:49 -0400

    Added pc_parts to routing

--- public/Javascript/app.js
+++ public/Javascript/app.js
@@ -2,14 +2,15 @@ var app = angular.module('pc_hobby', [
     'ngRoute'
 ]).
 config(['$routeProvider', function($routeProvider) {
-    $routeProvider. 
-    when("/questionaire", {templateUrl: "/questionaire.html", controller: ""}).
-    otherwise({redirectTo: '/questionaire'});
+    $routeProvider
+        .when("/questionaire", {templateUrl: "questionaire.html", controller: "pc_Controller"})
+        .when("/pc_parts", {templateUrl: "pc_parts.html", controller: "pc_Controller"})
+        .otherwise({redirectTo: '/questionaire'});
 }]);
 
 
 app.controller('pc_Controller', function(){
-    this.questionaire = question;
-    this.parts = parts;
+    //this.questionaire = question;
+    //this.parts = parts;
 })
 
