defe646544e0e4575b8cc6f1cb18e4e7b419c5f4
Nathaniel Salazar <nlpsalazar@gmail.com>
2016-07-27 12:31:23 -0400

    Prototyping the redirect function

--- public/Javascript/app.js
+++ public/Javascript/app.js
@@ -4,15 +4,21 @@ var app = angular.module('pc_hobby', [
 config(['$routeProvider', function($routeProvider) {
     $routeProvider
         .when("/questionaire", {templateUrl: "questionaire.html", controller: "pc_Controller", controllerAs: "pcCtrl"})
-        .when("/pc_parts", {templateUrl: "pc_parts.html", controller: "pc_Controller"})
+        .when("/pc_parts", {templateUrl: "pc_parts.html", controller: "parts_Controller", controllerAs: "partsCtrl"})
         .otherwise({redirectTo: '/questionaire'});
 }]);
 
 
-app.controller('pc_Controller', function(){
+app.controller('pc_Controller', function($location){
     this.answer = "gaming";
     this.submitAnswer = function(answer){
         alert(answer);
     };
+
+    var path = $location.path();
+});
+
+parts.controller('parts_Controller', function() {
+
 });
 
