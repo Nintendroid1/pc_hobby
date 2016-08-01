c8e2cc4f6f0aec0d61e0216b252debf8a08f97db
Chris Story <storytimesolutions@gmail.com>
2016-08-01 14:45:23 -0400

    I am the CSS king!

--- public/Javascript/functions.js
+++ public/Javascript/functions.js
@@ -2,4 +2,6 @@ function bgPicker()
 {
     var bgNum = Math.floor(Math.random() * 5) + 1;
     document.body.style.backgroundImage = "url('Pictures/bg"+bgNum+".jpg')";
+    document.body.style.backgroundAttachment = "fixed"
+    document.body.style.backgroundSize ="100% 100%";
 }
